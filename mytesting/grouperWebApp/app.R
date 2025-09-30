09aeaf3332dde25c7e7210ac12e29a35dc17647b
Vik <viknesh.gopal@gmail.com>
2025-09-30 20:12:50 +0800

    Shiny app step 1 for DBA roughly ok.

--- mytesting/grouperWebApp/app.R
+++ mytesting/grouperWebApp/app.R
@@ -1,21 +1,66 @@
 library(shiny)
 library(bslib)
+library(DT)
 library(magrittr)
 library(grouper)
 library(ompr)
 library(ompr.roi)
 library(ROI.plugin.glpk)
 
+library(readxl)
+
 ui <- page_navbar(
   nav_panel("Optimise",
             h2("Model preparation"),
             tabsetPanel(
               tabPanel("Diversity-based",
-                       fluidRow(
-                        h3("Student information input"),
-                        fileInput("stud_info", "Upload a file:"),
+                        br(),
+                        h3("Step 1: Student information input"),
+
+                        p('Upload a csv or excel file that contains information on the
+                          students in your course. There should be one row for each
+                          student. There should also be a column containing the
+                          groupings. For this diversity-based assignment, there
+                          may also be columns corresponding to demographics, and
+                          a single column corresponding to a skill factor.'),
+
+                        fluidRow(
+                          column(width=4, fileInput("stud_info", "Upload a file:",
+                                              accept=c(".csv", ".xlsx")) ),
+                          column(width=6, DTOutput("stud_info_preview"))
+                        ),
+
+                        fluidRow(
+                          column(width=4,
+                          "Select the column corresponding to the groups.",
+                          uiOutput("group_selection")
+                          ),
+
+                          column(width=4,
+                          "Select the column(s) corresponding to the demographic
+                          variables:",
+                          uiOutput("demographic_selection")
+                          ),
+
+                          column(width=4,
+                          "Select the column corresponding to the skill:",
+                          uiOutput("skill_selection")
+                          ),
+
+                        ),
+
+                        fluidRow(
+                          column(width=4, "Verify column selection"),
+                          column(width=6, "Columns verified")
+                        ),
+
+                        hr(),
+                        h3("Step 2: Model parameters"),
+                        p('Select the parameters for your model here, e.g. number
+                          of topics, number of members, etc.'),
+
+
                         actionButton("prepare", "Prepare model:")
-                       ),
               ),
               tabPanel("Preference-based", "Placeholder")
             ),
@@ -26,6 +71,8 @@ ui <- page_navbar(
             verbatimTextOutput("file1_contents"),
             textOutput("model_prep"),
             textOutput("optimised"),
+            actionButton("merge", "Merge dataframes"),
+            textOutput("merged"),
             ),
   nav_panel("Documentation",
             includeMarkdown("help.md")),
@@ -34,7 +81,50 @@ ui <- page_navbar(
   theme = bs_theme(bootswatch = "minty")
 )
 
-server <- function(input, output) {
+server <- function(input, output, session) {
+
+  stud_info_df <- reactive({
+    fname <- input$stud_info$datapath
+    f_ext <- tools::file_ext(fname)
+    if(f_ext == "csv"){
+      df <- read.csv(fname)
+    } else if (f_ext == "xlsx"){
+      df <- read_excel(fname)
+    }
+    df
+  })
+
+  output$stud_info_preview <- renderDT({
+    df <- input$stud_info
+    req(df)
+    stud_info_df()
+    # NULL
+  }, options=list(scrollX = TRUE))
+
+  output$group_selection <- renderUI({
+    df <- input$stud_info
+    req(df)
+    col_names <- colnames(stud_info_df())
+    selectizeInput("group_var", label="Column names:",
+                   choices=col_names, multiple=FALSE)
+  })
+
+  output$demographic_selection <- renderUI({
+    df <- input$stud_info
+    req(df)
+    col_names <- colnames(stud_info_df())
+    selectizeInput("demographic_vars", label="Column names:",
+                   choices=col_names, multiple=TRUE)
+  })
+
+  output$skill_selection <- renderUI({
+    df <- input$stud_info
+    req(df)
+    col_names <- colnames(stud_info_df())
+    selectizeInput("skill_var", label="Column names:",
+                   choices=col_names, multiple=FALSE)
+  })
+
   m4 <- reactive({
     df_ex004_list <- extract_student_info(dba_gc_ex004,
                                           skills = 2,
@@ -53,6 +143,12 @@ server <- function(input, output) {
   }) %>%
     bindEvent(input$optimise)
 
+  merged_df <- reactive({
+    assign_groups(result(), "diversity", dba_gc_ex004,
+                  group_names="self_groups")
+  }) %>%
+    bindEvent(input$merge)
+
   output$file1_contents <- renderPrint({print(input$stud_info)})
 
   output$model_prep <- renderText({
@@ -63,6 +159,11 @@ server <- function(input, output) {
   output$optimised <- renderText({
     result()
     return("Model optimised!")
+  })
+
+  output$merged <- renderText({
+    merged_df()
+    return("Df merged.")
   })
 
 }
